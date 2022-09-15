09ab24c7674d6dbc18bf75ca2ef12f211e7cd4fc
Ast415 <bastienbories81@gmail.com>
2022-09-15 11:01:37 +0200

    Creation et test de la classe Adhérent (avec la classe Program)

---
+++ src/com/btssio/projet1/classe/Adherent.java
@@ -0,0 +1,113 @@
+package com.btssio.projet1.classe;
+
+public class Adherent {
+	
+	private String nom;
+	private String prenom;
+	private String genre;
+	private String dateDeNaissance;
+	private String villeDeNaissance;
+	private String nomDeNaissance;
+	private String ville;
+	private String adresse;
+	private int cp;
+	private String nationalité;
+	private String numeroDeTelephone1;
+	private String numeroDeTelephone2;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+	public void setDateDeNaissance(String dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+	public String getVilleDeNaissance() {
+		return villeDeNaissance;
+	}
+	public void setVilleDeNaissance(String villeDeNaissance) {
+		this.villeDeNaissance = villeDeNaissance;
+	}
+	public String getNomDeNaissance() {
+		return nomDeNaissance;
+	}
+	public void setNomDeNaissance(String nomDeNaissance) {
+		this.nomDeNaissance = nomDeNaissance;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	public String getNationalité() {
+		return nationalité;
+	}
+	public void setNationalité(String nationalité) {
+		this.nationalité = nationalité;
+	}
+	public String getNumeroDeTelephone1() {
+		return numeroDeTelephone1;
+	}
+	public void setNumeroDeTelephone1(String numeroDeTelephone1) {
+		this.numeroDeTelephone1 = numeroDeTelephone1;
+	}
+	public String getNumeroDeTelephone2() {
+		return numeroDeTelephone2;
+	}
+	public void setNumeroDeTelephone2(String numeroDeTelephone2) {
+		this.numeroDeTelephone2 = numeroDeTelephone2;
+	}
+	
+	
+	public Adherent(String nom, String prenom, String genre, String dateDeNaissance, String villeDeNaissance,
+			String nomDeNaissance, String ville, String adresse, int cp, String nationalité, String numeroDeTelephone1,
+			String numeroDeTelephone2) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.genre = genre;
+		this.dateDeNaissance = dateDeNaissance;
+		this.villeDeNaissance = villeDeNaissance;
+		this.nomDeNaissance = nomDeNaissance;
+		this.ville = ville;
+		this.adresse = adresse;
+		this.cp = cp;
+		this.nationalité = nationalité;
+		this.numeroDeTelephone1 = numeroDeTelephone1;
+		this.numeroDeTelephone2 = numeroDeTelephone2;
+	}
+	
+	
+	
+
+}
---
+++ src/com/btssio/projet1/classe/Program.java
@@ -0,0 +1,21 @@
+package com.btssio.projet1.classe;
+
+public class Program {
+	public static void main(String[]args) {
+		Adherent Adh1 = new Adherent(
+				"BORIES",
+				"Bastien",
+				"Homme",
+				"29/01/2002",
+				"Castres",
+				"Bastien",
+				"Toulouse",
+				"118 route de narbone",
+				31400,
+				"Fr",
+				"0781878897",
+				"");
+		System.out.println(Adh1.getNom());
+	}
+
+}
