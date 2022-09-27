1249808dbc3a0f3f37dfe4951807b2496ad65569
Ast415 <bastienbories81@gmail.com>
2022-09-27 14:40:03 +0200

    Update JFformulaireAdherents.java
    Renommage des groupe
    creation de la liste des textF a saisir obligatoirement
    methode qui verifie que la valeur saisi dans le champ est une valeur
    methode verifie que certain champ doive etre complete

--- src/com/btssio/projet1/graphique/JFformulaireAdherents.java
+++ src/com/btssio/projet1/graphique/JFformulaireAdherents.java
@@ -1,5 +1,7 @@
 package com.btssio.projet1.graphique;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -15,6 +17,7 @@ import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JCheckBox;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JMenu;
 import javax.swing.ButtonGroup;
 import javax.swing.JList;
@@ -23,13 +26,18 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextArea;
 import javax.swing.JFormattedTextField;
 import javax.swing.JSeparator;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JFformulaireAdherents extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtfAdhNom;
 	private JTextField txtfAdhPrenom;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField txtfAdhNomNaiss;
 	private JTextField txtfAdhDateNaissJ;
 	private JTextField txtfAdhDateNaissM;
@@ -43,12 +51,16 @@ public class JFformulaireAdherents extends JFrame {
 	private JTextField txtfAdhTel2;
 	private JTextField txtfAdhCourriel;
 	private JTextField txtfAdhNomPrenomLeg;
-	private final ButtonGroup buttonGroup_1 = new ButtonGroup();
-	private final ButtonGroup buttonGroup_2 = new ButtonGroup();
-	private final ButtonGroup buttonGroup_3 = new ButtonGroup();
-	private final ButtonGroup buttonGroup_4 = new ButtonGroup();
-	private final ButtonGroup buttonGroup_5 = new ButtonGroup();
-	private final ButtonGroup buttonGroup_6 = new ButtonGroup();
+	private final ButtonGroup bgrGenre = new ButtonGroup();
+	private final ButtonGroup bgrArme = new ButtonGroup();
+	private final ButtonGroup bgrLateralite = new ButtonGroup();
+	private final ButtonGroup bgrFFE = new ButtonGroup();
+	private final ButtonGroup bgrAssurence = new ButtonGroup();
+	private final ButtonGroup bgrReducFamille = new ButtonGroup();
+	private final ButtonGroup bgrSeanceTir = new ButtonGroup();
+	
+	private List<JTextField> LesTxtfASaisir = new ArrayList<JTextField>();
+	private boolean formulaireComplete;
 
 	/**
 	 * Launch the application.
@@ -64,6 +76,50 @@ public class JFformulaireAdherents extends JFrame {
 				}
 			}
 		});
+	}
+	
+	public void AjoutDesTxtfASaisir() {
+		LesTxtfASaisir.add(txtfAdhNom);
+		LesTxtfASaisir.add(txtfAdhPrenom);
+		LesTxtfASaisir.add(txtfAdhAdresse);
+		LesTxtfASaisir.add(txtfAdhDateNaissJ);
+		LesTxtfASaisir.add(txtfAdhDateNaissM);
+		LesTxtfASaisir.add(txtfAdhDateNaissA);
+		LesTxtfASaisir.add(txtfAdhPaysNaiss);
+		LesTxtfASaisir.add(txtfAdhVilleNaiss);
+		LesTxtfASaisir.add(txtfAdhCP);
+		LesTxtfASaisir.add(txtfAdhVille);
+		LesTxtfASaisir.add(txtfAdhTel1);
+		LesTxtfASaisir.add(txtfAdhTel2);
+		LesTxtfASaisir.add(txtfAdhCourriel);
+	}
+	
+	
+	public boolean testSiInt(JTextField UnChampDeText) {
+		try {
+			int i = Integer.valueOf(UnChampDeText.getText());
+			return true;
+		}catch (Exception e){
+			return false;
+		}
+	}
+	
+	public void informationComplete(JTextField UnChampDeText) {
+		if (UnChampDeText.getText().equals("")){//Si les champ est vide
+			//JOptionPane.showMessageDialog(UnChampDeText,"Il n'y a rien de saisi dans ce champ.");
+			UnChampDeText.setBackground(Color.RED);//Change la couleur de l'arriere plan pour indiquer l'erreur
+			UnChampDeText.addMouseListener(new MouseAdapter() {//Si on clique sur la zone de texte ciblé
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					UnChampDeText.setBackground(Color.WHITE);//Alors elle redivent blanche
+				}
+			});
+			formulaireComplete = false;
+		}
+	}
+	
+	public boolean informationCorrecte(JTextField UnChampDeText) {
+		return true;
 	}
 
 	/**
@@ -86,17 +142,23 @@ public class JFformulaireAdherents extends JFrame {
 		lblAdhTitreForm.setBounds(0, 10, 1287, 38);
 		contentPane.add(lblAdhTitreForm);
 		
-		txtfAdhNom = new JTextField();
+		txtfAdhNom = new JTextField();/*
+		txtfAdhNom.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtfAdhNom.setBackground(Color.WHITE);
+			}
+		});*/
 		txtfAdhNom.setBounds(107, 90, 179, 25);
 		contentPane.add(txtfAdhNom);
 		txtfAdhNom.setColumns(10);
 		
-		JLabel lblAdhNom = new JLabel("Nom");
+		JLabel lblAdhNom = new JLabel("Nom* :");
 		lblAdhNom.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhNom.setBounds(10, 91, 91, 19);
 		contentPane.add(lblAdhNom);
 		
-		JLabel lblAdhPrenom = new JLabel("Prenom :");
+		JLabel lblAdhPrenom = new JLabel("Prenom* :");
 		lblAdhPrenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhPrenom.setBounds(10, 127, 91, 19);
 		contentPane.add(lblAdhPrenom);
@@ -106,18 +168,18 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhPrenom.setBounds(107, 126, 179, 25);
 		contentPane.add(txtfAdhPrenom);
 		
-		JLabel lblAdhGenre = new JLabel("Genre :");
+		JLabel lblAdhGenre = new JLabel("Genre* :");
 		lblAdhGenre.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhGenre.setBounds(10, 168, 91, 19);
 		contentPane.add(lblAdhGenre);
 		
 		JRadioButton rdbtnAdhGenreM = new JRadioButton("Masculin");
-		buttonGroup.add(rdbtnAdhGenreM);
+		bgrGenre.add(rdbtnAdhGenreM);
 		rdbtnAdhGenreM.setBounds(107, 168, 179, 21);
 		contentPane.add(rdbtnAdhGenreM);
 		
 		JRadioButton rdbtnAdhGenreF = new JRadioButton("Féminin");
-		buttonGroup.add(rdbtnAdhGenreF);
+		bgrGenre.add(rdbtnAdhGenreF);
 		rdbtnAdhGenreF.setBounds(107, 198, 179, 21);
 		contentPane.add(rdbtnAdhGenreF);
 		
@@ -141,9 +203,9 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhDateNaissJ.setBounds(467, 125, 25, 25);
 		contentPane.add(txtfAdhDateNaissJ);
 		
-		JLabel lblAdhDateNaiss = new JLabel("Date de naissance :");
+		JLabel lblAdhDateNaiss = new JLabel("Date de naissance* :");
 		lblAdhDateNaiss.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhDateNaiss.setBounds(324, 131, 133, 19);
+		lblAdhDateNaiss.setBounds(324, 131, 138, 19);
 		contentPane.add(lblAdhDateNaiss);
 		
 		txtfAdhDateNaissM = new JTextField();
@@ -161,12 +223,12 @@ public class JFformulaireAdherents extends JFrame {
 		lblAdhDateNaissSlach2.setBounds(531, 126, 12, 24);
 		contentPane.add(lblAdhDateNaissSlach2);
 		
-		JLabel lblAdhPaysNaiss = new JLabel("Pays de naissance :");
+		JLabel lblAdhPaysNaiss = new JLabel("Pays de naissance* :");
 		lblAdhPaysNaiss.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhPaysNaiss.setBounds(324, 168, 133, 19);
+		lblAdhPaysNaiss.setBounds(324, 168, 138, 19);
 		contentPane.add(lblAdhPaysNaiss);
 		
-		JLabel lblAdhVilleNaiss = new JLabel("Ville de naissance :");
+		JLabel lblAdhVilleNaiss = new JLabel("Ville de naissance* :");
 		lblAdhVilleNaiss.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhVilleNaiss.setBounds(324, 197, 133, 19);
 		contentPane.add(lblAdhVilleNaiss);
@@ -181,7 +243,7 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhVilleNaiss.setBounds(467, 199, 133, 25);
 		contentPane.add(txtfAdhVilleNaiss);
 		
-		JLabel lblAdhAdresse = new JLabel("Adresse :");
+		JLabel lblAdhAdresse = new JLabel("Adresse* :");
 		lblAdhAdresse.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhAdresse.setBounds(10, 270, 91, 19);
 		contentPane.add(lblAdhAdresse);
@@ -191,8 +253,8 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhAdresse.setBounds(107, 269, 493, 25);
 		contentPane.add(txtfAdhAdresse);
 		
-		JLabel lblAdhCP = new JLabel("Code Postal :");
-		lblAdhCP.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		JLabel lblAdhCP = new JLabel("Code Postal* :");
+		lblAdhCP.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblAdhCP.setBounds(10, 240, 91, 19);
 		contentPane.add(lblAdhCP);
 		
@@ -201,9 +263,9 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhCP.setBounds(107, 234, 68, 25);
 		contentPane.add(txtfAdhCP);
 		
-		JLabel lblAdhVille = new JLabel("Ville :");
+		JLabel lblAdhVille = new JLabel("Ville* :");
 		lblAdhVille.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhVille.setBounds(185, 235, 37, 19);
+		lblAdhVille.setBounds(185, 235, 48, 19);
 		contentPane.add(lblAdhVille);
 		
 		txtfAdhVille = new JTextField();
@@ -216,9 +278,9 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhTel1.setBounds(107, 304, 187, 25);
 		contentPane.add(txtfAdhTel1);
 		
-		JLabel lblAdhTel1 = new JLabel("Telephone 1 :");
-		lblAdhTel1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhTel1.setBounds(10, 305, 94, 19);
+		JLabel lblAdhTel1 = new JLabel("Telephone 1* :");
+		lblAdhTel1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblAdhTel1.setBounds(10, 305, 99, 19);
 		contentPane.add(lblAdhTel1);
 		
 		txtfAdhTel2 = new JTextField();
@@ -226,12 +288,12 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhTel2.setBounds(413, 304, 187, 25);
 		contentPane.add(txtfAdhTel2);
 		
-		JLabel lblAdhTel2 = new JLabel("Telephone 2 :");
+		JLabel lblAdhTel2 = new JLabel("Telephone 2* :");
 		lblAdhTel2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhTel2.setBounds(309, 305, 94, 19);
+		lblAdhTel2.setBounds(309, 305, 105, 19);
 		contentPane.add(lblAdhTel2);
 		
-		JLabel lblAdhCourriel = new JLabel("Courriel :");
+		JLabel lblAdhCourriel = new JLabel("Courriel* :");
 		lblAdhCourriel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhCourriel.setBounds(10, 340, 91, 19);
 		contentPane.add(lblAdhCourriel);
@@ -251,127 +313,127 @@ public class JFformulaireAdherents extends JFrame {
 		txtfAdhNomPrenomLeg.setBounds(10, 399, 590, 25);
 		contentPane.add(txtfAdhNomPrenomLeg);
 		
-		JLabel lblAdhArme = new JLabel("Arme :");
+		JLabel lblAdhArme = new JLabel("Arme* :");
 		lblAdhArme.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhArme.setBounds(10, 434, 91, 19);
 		contentPane.add(lblAdhArme);
 		
 		JCheckBox rdbtnAdhArmeF = new JCheckBox("Fleuret");
-		buttonGroup_1.add(rdbtnAdhArmeF);
+		bgrArme.add(rdbtnAdhArmeF);
 		rdbtnAdhArmeF.setBounds(107, 435, 93, 21);
 		contentPane.add(rdbtnAdhArmeF);
 		
 		JCheckBox rdbtnAdhArmeE = new JCheckBox("Epee");
-		buttonGroup_1.add(rdbtnAdhArmeE);
+		bgrArme.add(rdbtnAdhArmeE);
 		rdbtnAdhArmeE.setBounds(107, 458, 93, 21);
 		contentPane.add(rdbtnAdhArmeE);
 		
 		JCheckBox rdbtnAdhArmeS = new JCheckBox("Sabre");
-		buttonGroup_1.add(rdbtnAdhArmeS);
+		bgrArme.add(rdbtnAdhArmeS);
 		rdbtnAdhArmeS.setBounds(107, 481, 93, 21);
 		contentPane.add(rdbtnAdhArmeS);
 		
-		JLabel lblAdhPratique = new JLabel("Pratique :");
+		JLabel lblAdhPratique = new JLabel("Pratique* :");
 		lblAdhPratique.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhPratique.setBounds(261, 434, 91, 19);
 		contentPane.add(lblAdhPratique);
 		
-		JLabel lblAdhLateralite = new JLabel("Latéralité :");
+		JLabel lblAdhLateralite = new JLabel("Latéralité* :");
 		lblAdhLateralite.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdhLateralite.setBounds(261, 462, 91, 19);
 		contentPane.add(lblAdhLateralite);
 		
 		JCheckBox rdbtnAdhPratiqueL = new JCheckBox("Loisir");
-		buttonGroup.add(rdbtnAdhPratiqueL);
+		bgrGenre.add(rdbtnAdhPratiqueL);
 		rdbtnAdhPratiqueL.setBounds(358, 435, 93, 21);
 		contentPane.add(rdbtnAdhPratiqueL);
 		
 		JCheckBox rdbtnAdhPratiqueC = new JCheckBox("Compétition");
-		buttonGroup.add(rdbtnAdhPratiqueC);
+		bgrGenre.add(rdbtnAdhPratiqueC);
 		rdbtnAdhPratiqueC.setBounds(453, 435, 126, 21);
 		contentPane.add(rdbtnAdhPratiqueC);
 		
 		JCheckBox rdbtnAdhLateraliteD = new JCheckBox("Droitier");
-		buttonGroup_2.add(rdbtnAdhLateraliteD);
+		bgrLateralite.add(rdbtnAdhLateraliteD);
 		rdbtnAdhLateraliteD.setBounds(358, 463, 93, 21);
 		contentPane.add(rdbtnAdhLateraliteD);
 		
 		JCheckBox rdbtnAdhLateraliteG = new JCheckBox("Gaucher");
-		buttonGroup_2.add(rdbtnAdhLateraliteG);
+		bgrLateralite.add(rdbtnAdhLateraliteG);
 		rdbtnAdhLateraliteG.setBounds(453, 463, 126, 21);
 		contentPane.add(rdbtnAdhLateraliteG);
 		
 		JLabel lblAdhFFE = new JLabel("Licence FFE :");
 		lblAdhFFE.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhFFE.setBounds(674, 90, 91, 19);
+		lblAdhFFE.setBounds(988, 90, 91, 19);
 		contentPane.add(lblAdhFFE);
 		
 		JCheckBox rdbtnAdhFFEO = new JCheckBox("Oui");
-		buttonGroup_3.add(rdbtnAdhFFEO);
-		rdbtnAdhFFEO.setBounds(771, 91, 59, 21);
+		bgrFFE.add(rdbtnAdhFFEO);
+		rdbtnAdhFFEO.setBounds(1085, 91, 59, 21);
 		contentPane.add(rdbtnAdhFFEO);
 		
 		JCheckBox rdbtnAdhFFEN = new JCheckBox("Non");
-		buttonGroup_3.add(rdbtnAdhFFEN);
-		rdbtnAdhFFEN.setBounds(832, 91, 59, 21);
+		bgrFFE.add(rdbtnAdhFFEN);
+		rdbtnAdhFFEN.setBounds(1146, 91, 59, 21);
 		contentPane.add(rdbtnAdhFFEN);
 		
 		JLabel lblAdhAssurance = new JLabel("Assurance :");
 		lblAdhAssurance.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhAssurance.setBounds(674, 115, 91, 19);
+		lblAdhAssurance.setBounds(988, 115, 91, 19);
 		contentPane.add(lblAdhAssurance);
 		
 		JCheckBox rdbtnAdhAssurenceO = new JCheckBox("Oui");
-		buttonGroup_4.add(rdbtnAdhAssurenceO);
-		rdbtnAdhAssurenceO.setBounds(771, 116, 59, 21);
+		bgrAssurence.add(rdbtnAdhAssurenceO);
+		rdbtnAdhAssurenceO.setBounds(1085, 116, 59, 21);
 		contentPane.add(rdbtnAdhAssurenceO);
 		
 		JCheckBox rdbtnAdhAssuranceN = new JCheckBox("Non");
-		buttonGroup_4.add(rdbtnAdhAssuranceN);
-		rdbtnAdhAssuranceN.setBounds(832, 116, 59, 21);
+		bgrAssurence.add(rdbtnAdhAssuranceN);
+		rdbtnAdhAssuranceN.setBounds(1146, 116, 59, 21);
 		contentPane.add(rdbtnAdhAssuranceN);
 		
 		JLabel lblAdhReducFamille = new JLabel("Reduction famille :");
 		lblAdhReducFamille.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhReducFamille.setBounds(988, 90, 133, 19);
+		lblAdhReducFamille.setBounds(988, 186, 133, 19);
 		contentPane.add(lblAdhReducFamille);
 		
 		JCheckBox rdbtnAdhReducFO = new JCheckBox("Oui");
-		buttonGroup_5.add(rdbtnAdhReducFO);
-		rdbtnAdhReducFO.setBounds(1144, 91, 59, 21);
+		bgrReducFamille.add(rdbtnAdhReducFO);
+		rdbtnAdhReducFO.setBounds(1144, 187, 59, 21);
 		contentPane.add(rdbtnAdhReducFO);
 		
 		JCheckBox rdbtnAdhReducFN = new JCheckBox("Non");
-		buttonGroup_5.add(rdbtnAdhReducFN);
-		rdbtnAdhReducFN.setBounds(1205, 91, 59, 21);
+		bgrReducFamille.add(rdbtnAdhReducFN);
+		rdbtnAdhReducFN.setBounds(1205, 187, 59, 21);
 		contentPane.add(rdbtnAdhReducFN);
 		
 		JLabel lblAdhReducFText = new JLabel("Si oui vous est le");
 		lblAdhReducFText.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhReducFText.setBounds(988, 120, 127, 19);
+		lblAdhReducFText.setBounds(988, 216, 127, 19);
 		contentPane.add(lblAdhReducFText);
 		
 		JComboBox cmbAdhReducFamille = new JComboBox();
-		cmbAdhReducFamille.setModel(new DefaultComboBoxModel(new String[] {"2e adhérent", "3e adhérent ou plus"}));
-		cmbAdhReducFamille.setBounds(1104, 121, 160, 21);
+		cmbAdhReducFamille.setModel(new DefaultComboBoxModel(new String[] {"Rien", "2e adhérent", "3e adhérent ou plus"}));
+		cmbAdhReducFamille.setBounds(1104, 217, 160, 21);
 		contentPane.add(cmbAdhReducFamille);
 		
 		JLabel lblAdhSeancesTir = new JLabel("10 Seances pour tireurs : ");
 		lblAdhSeancesTir.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAdhSeancesTir.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdhSeancesTir.setBounds(853, 198, 212, 19);
+		lblAdhSeancesTir.setBounds(674, 186, 212, 19);
 		contentPane.add(lblAdhSeancesTir);
 		
 		JCheckBox rdbtnAdhSeanceTirO = new JCheckBox("Oui");
-		buttonGroup_6.add(rdbtnAdhSeanceTirO);
+		bgrSeanceTir.add(rdbtnAdhSeanceTirO);
 		rdbtnAdhSeanceTirO.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnAdhSeanceTirO.setBounds(853, 223, 93, 21);
+		rdbtnAdhSeanceTirO.setBounds(674, 211, 93, 21);
 		contentPane.add(rdbtnAdhSeanceTirO);
 		
 		JCheckBox rdbtnAdhSeanceTirN = new JCheckBox("Non");
-		buttonGroup_6.add(rdbtnAdhSeanceTirN);
+		bgrSeanceTir.add(rdbtnAdhSeanceTirN);
 		rdbtnAdhSeanceTirN.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnAdhSeanceTirN.setBounds(972, 223, 93, 21);
+		rdbtnAdhSeanceTirN.setBounds(793, 211, 93, 21);
 		contentPane.add(rdbtnAdhSeanceTirN);
 		
 		JLabel lblAdhAideMobil = new JLabel("Dispositif d'aide mobilisé");
@@ -385,13 +447,60 @@ public class JFformulaireAdherents extends JFrame {
 		contentPane.add(txtaAdhAideMobil);
 		
 		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				formulaireComplete = true;
+				AjoutDesTxtfASaisir();
+				//JOptionPane.showMessageDialog(txtfAdhNom,informationComplete(txtfAdhPrenom));
+				//informationComplete(txtfAdhNom);
+				for( Component comp : contentPane.getComponents()) {/*comp sera a tour de role associer a chaque composent du formulaire*/
+					if( comp instanceof JTextField) {/*Si comp est un element JTextField (les zone de saisie de texte)*/
+						if (LesTxtfASaisir.contains((JTextField)comp)) {
+							informationComplete((JTextField)comp);
+						}
+					}
+				}
+				if (formulaireComplete == false) {
+					JOptionPane.showMessageDialog(contentPane,"ERREUR : Le formulaire n'est pas totalement complété.");
+				}
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnNewButton.setBounds(440, 568, 160, 38);
 		contentPane.add(btnNewButton);
 		
-		JButton btnRinitialiser = new JButton("Réinitialiser");
-		btnRinitialiser.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnRinitialiser.setBounds(674, 568, 160, 38);
-		contentPane.add(btnRinitialiser);
+		JButton btnReinitialiser = new JButton("Réinitialiser");
+		btnReinitialiser.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				for( Component comp : contentPane.getComponents()) {/*comp sera a tour de role associer a chaque composent du formulaire*/
+					if( comp instanceof JTextField) {/*Si comp est un element JTextField (les zone de saisie de texte)*/
+						((JTextField)comp).setText(null);/*Definit le JtextField equivalent a comp, comme null*/
+						((JTextField)comp).setBackground(Color.WHITE);
+					}
+					if(comp instanceof JTextArea) {
+						((JTextArea)comp).setText(null);
+					}
+				}
+			}
+		});
+		btnReinitialiser.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnReinitialiser.setBounds(674, 568, 160, 38);
+		contentPane.add(btnReinitialiser);
+		
+		JLabel lblCategorie = new JLabel("Categorie* :");
+		lblCategorie.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCategorie.setBounds(674, 91, 77, 19);
+		contentPane.add(lblCategorie);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Éveil", "Atomes", "Poussins", "Pupilles", "Benjamins", "Minimes", "Cadets", "Juniors", "Séniors", "Vétérans"}));
+		comboBox.setBounds(761, 91, 179, 22);
+		contentPane.add(comboBox);
+		
+		JLabel lblAdhNom_1 = new JLabel("INFORMATION : Les éléments contenant un * doit être complété pour valider le formulaire.");
+		lblAdhNom_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblAdhNom_1.setBounds(10, 545, 590, 19);
+		contentPane.add(lblAdhNom_1);
 	}
 }
