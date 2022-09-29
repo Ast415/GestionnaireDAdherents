10272c99f88307bacc3c200d3b18565b3e54d5c4
Ast415 <bastienbories81@gmail.com>
2022-09-29 08:26:43 +0200

    reset de Program

--- src/com/btssio/projet1/classe/Program.java
+++ src/com/btssio/projet1/classe/Program.java
@@ -8,25 +8,8 @@ import org.xml.sax.SAXException;
 
 public class Program {
 	public static void main(String[]args) throws ParserConfigurationException, SAXException {
-		Adherent Adh1 = new Adherent(
-				"BORIES",
-				"Bastien",
-				"Homme",
-				"29/01/2002",
-				"Castres",
-				"Bastien",
-				"Toulouse",
-				"118 route de narbone",
-				31400,
-				"Fr",
-				"0781878897",
-				"0000000000",
-				"Epee",
-				"Escrime",
-				"Droitier");
-		ArrayList<Club> lesClub = new ArrayList();
-		lesClub = lectureXML.importationXMLClub();
-		for (Club elem : lesClub) { System.out.print(elem.getNom()+", "); }
+		
+		
 	}
 
 }
