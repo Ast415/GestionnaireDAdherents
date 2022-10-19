aed9dd30da50960046b31443fc0a6c1132f82b97
Ast415 <bastienbories81@gmail.com>
2022-10-20 00:19:35 +0200

    Update Program.java

--- src/com/btssio/projet1/classe/Program.java
+++ src/com/btssio/projet1/classe/Program.java
@@ -1,10 +1,15 @@
 package com.btssio.projet1.classe;
 
+import java.net.URISyntaxException;
+
 import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
+
+
+
 
 public class Program {
 	public static void main(String[]args) throws ParserConfigurationException, SAXException {
